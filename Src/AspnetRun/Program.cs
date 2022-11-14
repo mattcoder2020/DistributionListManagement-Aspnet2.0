4e65e3837e0f0096248c17a9692826d99c5b6b32
mattcoder2017 <myang@arris.com>
2022-11-14 14:59:58 +0800

    Init change

---
+++ Src/AspnetRun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspnetRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
